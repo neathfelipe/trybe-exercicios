9a4173f65b679739b13cdd85fac263914b4c3080
Felipe Rodrigues <neath.felipe@gmail.com>
2023-05-02 23:04:27 -0300

    Explica os arquivos

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratice/script-types.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratice/script-types.js
@@ -1,3 +1,8 @@
+/* Crie uma variável chamada base e atribua a ela o valor 5.
+Crie uma variável chamada height e atribua a ela o valor 8.
+Crie uma variável chamada area e atribua a ela o resultado da multiplicação da base pela height. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+Crie uma variável chamada perimeter e atribua a ela a soma de todos os lados do retângulo. */
+
 let base = 5;
 let height = 8;
 let area = base * height;
--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratice/script-variaveis.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratice/script-variaveis.js
@@ -1,3 +1,8 @@
+/*Crie uma variável chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+Crie uma variável chamada birthCity e atribua a ela a sua cidade natal.
+Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+Utilize o console.log() para imprimir as variáveis que você criou.*/
+
 let myName = "Felipe";
 let birthCity = "Belo Horizonte";
 let birthYear = 1996;
