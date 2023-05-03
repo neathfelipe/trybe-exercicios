81714321b4811c5579eb792265caf68b42ad99b2
Felipe Rodrigues <neath.felipe@gmail.com>
2023-05-03 01:04:23 -0300

    Termina o exercicio 6

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercise/ex05.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercise/ex05.js
@@ -1,11 +1,16 @@
 /* Utilize if/else para escrever um código que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro. */
 
 let angulo1 = 50;
-let angulo2 = 70;
-let angulo3 = 60;
+let angulo2 = 90;
+let angulo3 = 50;
+let angulosPositivos = angulo1 > 0 && angulo2 > 0 && angulo3 > 0;
 
-if ((angulo1 + angulo2 + angulo3) <= 180) {
-  console.log('true');
+if (angulosPositivos) {
+  if ((angulo1 + angulo2 + angulo3) === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 } else {
-  console.log('false');
+  console.log('Erro: Angulo errado');
 }
