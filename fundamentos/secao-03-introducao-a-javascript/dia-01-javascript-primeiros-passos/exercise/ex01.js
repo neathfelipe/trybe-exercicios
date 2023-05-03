52e7cd5098e88646cfdb873f4d9c66038d533a04
Felipe Rodrigues <neath.felipe@gmail.com>
2023-05-03 00:11:06 -0300

    Termina o exercicio 2

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercise/ex01.js
@@ -0,0 +1,9 @@
+/* 1 - Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. */
+let a = 10;
+let b = 15;
+
+console.log("A soma desses numeros é " + (a + b));
+console.log("A subtração desses numeros é " + (a - b));
+console.log("A multiplicação desses numeros é " + (a * b));
+console.log("A divisao desses numeros é " + (a / b).toFixed(2));
+console.log("O modulo desses numeros é " + (a % b));
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercise/ex02.js
@@ -0,0 +1,11 @@
+/* 2 - Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.*/
+let a = 50;
+let b = 50;
+
+if (a > b) {
+  console.log('O numero ' + a + ' é maior que ' + b);
+} else if (a < b) {
+  console.log('O numero ' + b + ' é maior que ' + a);
+} else {
+  console.log('Os numeros ' + a + ' e ' + b + ' são iguais');
+}
