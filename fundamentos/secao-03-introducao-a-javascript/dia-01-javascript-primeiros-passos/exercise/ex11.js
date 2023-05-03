83c13c3eb41c17c60db5322588f220f24150315c
Felipe Rodrigues <neath.felipe@gmail.com>
2023-05-03 03:27:30 -0300

    Reorganiza o  exercicio 11

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercise/ex11.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercise/ex11.js
@@ -23,50 +23,35 @@ Para obter o valor do imposto de renda, calcula-se: R$ 200,25 (que representa 7,
 Para obter o salário líquido, calcula-se: R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
 Resultado: R$ 2.612,55. */
 
-let salarioBruto = 3000;
-let impostoDeRenda;
-let inss;
-let salarioBase;
-let aliquiotaImposto;
 
-if (salarioBruto <= 1556.94) {
-  inss = 0.08
-} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-  inss = 0.09
-} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
-  inss = 0.11
+let descInss;
+let aliquotImpost;
+
+let salarioBruto = 10000.00;
+
+if (salarioBruto
+  <= 1556.94) {
+  descInss = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  descInss = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  descInss = salarioBruto * 0.11;
+} else {
+  descInss = 570.88;
 }
 
-if (salarioBruto > 5189.83) {
-  salarioBase = salarioBruto - 570.88;
-} else if (salarioBruto >= 0 && salarioBruto <= 5189.82) {
-  salarioBase = salarioBruto - (salarioBruto * inss);
-}
+let salarioBase = salarioBruto - descInss;
 
 if (salarioBase <= 1903.98) {
-  aliquiotaImposto = 0;
-} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
-  aliquiotaImposto = 142.80;
-} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
-  aliquiotaImposto = 354.80;
-} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
-  aliquiotaImposto = 636.13;
-} else if (salarioBase > 4664.68) {
-  aliquiotaImposto = 869.36;
-}
+  aliquotImpost = 0;
+} else if (salarioBase <= 2826.65) {
+  aliquotImpost = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase <= 3751.05) {
+  aliquotImpost = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68) {
+  aliquotImpost = (salarioBase * 0.225) - 636.13;
+} else {
+  aliquotImpost = (salarioBase * 0.275) - 869.36;
+};
 
-if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
-  impostoDeRenda = 0.075;
-} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
-  impostoDeRenda = 0.15;
-} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
-  impostoDeRenda = 0.225;
-} else if (salarioBase > 4664.68) {
-  impostoDeRenda = 0.275;
-}
-
-let descImposto = impostoDeRenda * salarioBase - aliquiotaImposto;
-
-let salarioLiquido = salarioBase - descImposto;
-
-console.log('Seu salário liquido vai ser de R$' + salarioLiquido.toFixed(2));
+console.log("Salário liquido: R$" + (salarioBase - aliquotImpost).toFixed(2));
