bc5119c8ae562f89cd8cbe02a2f45fc648e7adac
Felipe Rodrigues <neath.felipe@gmail.com>
2023-05-03 23:43:19 -0300

    Alterando valores

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercise/ex08.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercise/ex08.js
@@ -1,7 +1,7 @@
 /* Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false.*/
 
 let num1 = 5;
-let num2 = 7;
+let num2 = 2;
 let num3 = 1;
 
 if ((num1 % 2) == 0 || (num2 % 2) == 0 || (num3 % 2) == 0) {
