abb3e48b6ba2193417443b64fbcda836ec121917
Felipe Rodrigues <neath.felipe@gmail.com>
2023-07-05 23:24:06 -0300

    Finalized Project Front-end 2.4

---
+++ front-end/secao-02-introducao-ao-react/dia-04-pratica-solar-system/src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Sistema Solar</h1>
+      <img src="/images/logo-sistema-solar.png" alt="fundo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ front-end/secao-02-introducao-ao-react/dia-04-pratica-solar-system/src/data/planets.ts
@@ -0,0 +1,21 @@
+import MercurioImg from '../images/Mercurio.png';
+import VenusImg from '../images/Venus.png';
+import TerraImg from '../images/Terra.png';
+import MarteImg from '../images/Marte.png';
+import JupiterImg from '../images/Jupiter.png';
+import SaturnoImg from '../images/Saturno.png';
+import Uranoimg from '../images/Urano.png';
+import Netunoimg from '../images/Netuno.png';
+
+const Planets = [
+  { name: 'Mercúrio', image: MercurioImg, height: '31px' },
+  { name: 'Vênus', image: VenusImg, height: '76px' },
+  { name: 'Terra', image: TerraImg, height: '79px' },
+  { name: 'Marte', image: MarteImg, height: '50px' },
+  { name: 'Júpiter', image: JupiterImg, height: '204px' },
+  { name: 'Saturno', image: SaturnoImg, height: '210px' },
+  { name: 'Urano', image: Uranoimg, height: '109px' },
+  { name: 'Netuno', image: Netunoimg, height: '107px' },
+];
+
+export default Planets;
