0054a3d56fb3aa4c54057e57794fb9502f7f7efe
Felipe Rodrigues <neath.felipe@gmail.com>
2023-07-07 18:00:53 -0300

    Finalized Pratice Front-end 2.5

---
+++ front-end/secao-02-introducao-ao-react/dia-05-estados-e-eventos/pratice/event-handlers/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+// src/App.tsx
+
+import Button from './components/Button';
+import Input from './components/Input';
+
+function App() {
+  return (
+    <>
+    <Button />
+    <Input />
+    </>
+  )
+}
+
+export default App;
