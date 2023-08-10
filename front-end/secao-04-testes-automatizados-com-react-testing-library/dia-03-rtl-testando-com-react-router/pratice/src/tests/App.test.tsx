ec19e7b4abec7fb1b24650d4fef36f19cba93310
Felipe Rodrigues <neath.felipe@gmail.com>
2023-08-09 22:49:47 -0300

    Finaly Day 4.3

--- front-end/secao-04-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-com-react-router/pratice/src/tests/App.test.tsx
+++ front-end/secao-04-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-com-react-router/pratice/src/tests/App.test.tsx
@@ -1,14 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import { renderWithRouter } from '../utils/renderWithRouter';
 
 import App from '../App';
 
-it('Navega para página About', async () => {
-  render(<App />, { wrapper: BrowserRouter });
+it('Navega para a página About', async () => {
+  const { user } = renderWithRouter(<App />);
+
   expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
 
-  const aboutLink = screen.getByRole('link', { name: /Sobre/i});
-  await userEvent.click(aboutLink);
+  const aboutLink = screen.getByRole('link', { name: /Sobre/i });
+  await user.click(aboutLink);
   expect(screen.getByText(/Você está na página Sobre/i)).toBeInTheDocument();
-})
+});
+
+it('Testa rota not found', () => {
+  renderWithRouter(<App />, { route: '/something-else' });
+
+  expect(screen.getByText(/Página não encontrada/i)).toBeInTheDocument();
+});
