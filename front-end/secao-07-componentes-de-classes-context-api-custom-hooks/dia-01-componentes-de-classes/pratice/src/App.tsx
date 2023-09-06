51d586228d9c575ed01adc9f2942b4ad486b74f8
Felipe Rodrigues <neath.felipe@gmail.com>
2023-09-06 19:08:04 -0300

    Finaliza o dia 7.1

---
+++ front-end/secao-07-componentes-de-classes-context-api-custom-hooks/dia-01-componentes-de-classes/pratice/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from './Title';
+
+class App extends React.Component {
+  render() {
+    return <Title titleMessage="Hello World" />;
+  }
+}
+
+export default App;
